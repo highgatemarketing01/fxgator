a1791de31282b6295f5dceb87601a185206dd2d2
highgatemarketing01 <lucas@highgatemarketing.com>
2025-11-28 19:26:42 +0000

    Updated package-lock.json

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
-import { ThankYouSignals } from './pages/ThankYouSignals.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/thank-you-signals" element={<ThankYouSignals />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
